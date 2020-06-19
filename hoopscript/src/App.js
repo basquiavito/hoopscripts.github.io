9e5f1114074a3b95e40b154db3816ec6fe8a83aa
basquiavito <54229601+basquiavito@users.noreply.github.com>
2020-06-19 17:02:24 -0400

    Update App.js

--- hoopscript/src/App.js
+++ hoopscript/src/App.js
@@ -4,10 +4,12 @@ import './App.css';
 
 function App() {
   return (
+    <>
     <div className="App">
     <h2>Welcome to HoopScript</h2>
     <p>The programming language of basketball</p>
-    </div>
+   </div>
+    </>
   );
 }
 
